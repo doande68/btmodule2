48636a380fb2141db7e036093476257f5ed7ce8f
doande68 <138882136+doande68@users.noreply.github.com>
2023-10-09 11:34:21 +0700

    Add files via upload

--- Quanlysp/src/View/Menu.java
+++ Quanlysp/src/View/Menu.java
@@ -3,6 +3,7 @@ package View;
 import Manager.ProductManager;
 import Model.Product;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -23,7 +24,7 @@ public class Menu {
                     "5.Tìm kiếm các sản phẩm gần đúng \n" +
                     "6.Nhập vào sản phẩm và lấy ra các sản phẩm cùng loại \n " +
                     "7.Tìm sản phẩm theo id\n" +
-                    "8.Thoát trương trình \n");
+                    "0.Thoát trương trình \n");
             System.out.println("Nhập lựa chọn của bạn:");
             try {
                 choice = Integer.parseInt(inputStr.nextLine());
@@ -40,35 +41,121 @@ public class Menu {
                     case 4:
                         showMenuShow();
                         break;
-                    default:
+                    case 5:
+                        searchByName();
+                        break;
+                    case 6:
+                        searchCategorize();
+                        break;
+                    case 7:
+                        searchById();
+                        break;
+                    case 0:
                         System.out.println("Thoát chương trình thành công !!!");
                         break;
                 }
-            } catch (NumberFormatException exception) {
+            } catch (NumberFormatException ex) {
                 System.out.println("Nhập sai định dạng");
                 choice = -1;
             }
         } while (choice != 0);
     }
     public void showMenuAdd(){
-        System.out.println("Thêm sản phẩm :");
-        System.out.println("Nhập tên sản phẩm:");
-        String name = inputNum.nextLine();
-        System.out.println("Nhập số lượng sản phẩm : ");
-        int quantity = inputNum.nextInt();
-        System.out.println("Nhập giá sản phẩm :");
-        double price = inputNum.nextInt();
-        System.out.println("Nhập loại sản phẩm :");
+        System.out.println("\nĐang thêm mới sản phẩm ...");
+        System.out.println("Nhập tên sản phẩm: ");
+        String name = inputStr.nextLine();
+        int quantity;
+        while (true){
+            System.out.println("Nhập số lượng sản phẩm: ");
+            try{
+                quantity = Integer.parseInt(inputNum.nextLine());
+                break;
+            }catch (NumberFormatException ex){
+                System.out.println("Sai định dạng nhập lại :((");
+            }
+        }
+        double price;
+        while (true){
+            System.out.println("Nhập giá sản phẩm : ");
+            try {
+                price = Double.parseDouble(inputNum.nextLine());
+                break;
+            }catch (NumberFormatException ex){
+                System.out.println("Nhập sai định dạng nhập lại !!!");
+            }
+        }
+        System.out.print("Nhập loại sản phẩm: ");
         String categorize = inputStr.nextLine();
-        Product product = new Product(name , quantity,price,categorize);
+        Product product = new Product(name, quantity, price, categorize);
         listProduct.add(product);
-        System.out.println("Thêm thành công !!!\n");
+        System.out.println("Thêm thành công!!!\n");
     }
     public void showMenuEdit(){
+        System.out.println("\nĐang sửa sản phẩm....");
+        int id;
+        while (true) {
+            try {
+                System.out.print("Nhập id sản phẩm cần sửa: ");
+                id = Integer.parseInt(inputNum.nextLine());
+                break;
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        }
+        int index = listProduct.findIndex(id);
+        if (index == -1) {
+            System.out.println("Không có sản phẩm nào có id này!!!\n");
+            return;
+        }
+        System.out.print("Nhập tên sản phẩm: ");
+        String name = inputStr.nextLine();
+        int quantity;
+        while (true) {
+            System.out.print("Nhập số lượng sản phẩm: ");
+            try {
+                quantity = Integer.parseInt(inputNum.nextLine());
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Sai định dạng nhập lại!!!");
+            }
 
+        }
+        double price;
+        while (true) {
+            System.out.print("Nhập giá sản phẩm: ");
+            try {
+                price = Double.parseDouble(inputNum.nextLine());
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Sai định dạng nhập lại!!!");
+            }
+
+        }
+        System.out.print("Nhập loại sản phẩm: ");
+        String categorize = inputStr.nextLine();
+        Product product = new Product(id, name, quantity, price, categorize);
+        listProduct.edit(index, product);
+        System.out.println("Sửa thành công!!!\n");
     }
     public void showMenuDelete(){
-
+        System.out.println("\nĐang xóa sản phẩm....");
+        int id;
+        while (true) {
+            try {
+                System.out.print("Nhập id sản phẩm cần xóa: ");
+                id = Integer.parseInt(inputNum.nextLine());
+                break;
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        }
+        int index = listProduct.findIndex(id);
+        if (index == -1) {
+            System.out.println("Không có sản phẩm nào có id này!!!\n");
+            return;
+        }
+        listProduct.delete(index);
+        System.out.println("Xóa thành công!!!\n");
     }
     public void showMenuShow(){
         System.out.println("Danh sách sản phẩm hiện tại:");
@@ -79,4 +166,48 @@ public class Menu {
         }
         System.out.println();
    }
+   public void searchByName(){
+       System.out.println("\nTìm kiếm theo tên gần đúng...");
+       System.out.print("Nhập tên gần đúng: ");
+       String name = inputStr.nextLine();
+       for (Product product :
+               listProduct.showAll()) {
+           if (product.getName().toLowerCase().contains(name.toLowerCase())) {
+               System.out.println(product);
+           }
+       }
+       System.out.println();
+   }
+    public void searchCategorize() {
+        System.out.println("\nTìm kiếm theo loại sản phẩm...");
+        System.out.print("Nhập loại sản phẩm cần tìm: ");
+        String categorize = inputStr.nextLine();
+        for (Product product :
+                listProduct.showAll()) {
+            if (product.getCategorize().toLowerCase().contains(categorize.toLowerCase())) {
+                System.out.println(product);
+            }
+        }
+        System.out.println();
+    }
+    public void searchById() {
+        System.out.println("\nTìm kiếm theo id sản phẩm...");
+        int id;
+        while (true) {
+            try {
+                System.out.print("Nhập id sản phẩm cần sửa: ");
+                id = Integer.parseInt(inputNum.nextLine());
+                break;
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        }
+        int index = listProduct.findIndex(id);
+        if (index == -1) {
+            System.out.println("Không có sản phẩm nào có id này!!!\n");
+        } else {
+            System.out.println(listProduct.showAll().get(index));
+        }
+        System.out.println();
+    }
 }
